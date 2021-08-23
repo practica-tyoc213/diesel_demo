705be24888064b29638f525b1ebd950679c0e07b
tyoc213 <tyoc213+git@tyoc.mx>
2021-08-22 23:01:11 -0500

    Adding first paginated method, not working
    ```
    error[E0599]: the method `limit` exists for reference `&Paginated<T>`, but its trait bounds were not satisfied
      --> src/bin/extending_pager.rs:20:48
       |
    20 |         out.push_bind_param::<BigInt, _>(&self.limit())?;
       |                                                ^^^^^ method cannot be called on `&Paginated<T>` due to unsatisfied trait bounds
    ...
    50 | pub struct Paginated<T> {
       | -----------------------
       | |
       | doesn't satisfy `Paginated<T>: QueryDsl`
       | doesn't satisfy `Paginated<T>: Table`
       |
       = note: the following trait bounds were not satisfied:
               `&Paginated<T>: Table`
               which is required by `&Paginated<T>: QueryDsl`
               `&&Paginated<T>: Table`
               which is required by `&&Paginated<T>: QueryDsl`
               `&mut &Paginated<T>: Table`
               which is required by `&mut &Paginated<T>: QueryDsl`
               `Paginated<T>: Table`
               which is required by `Paginated<T>: QueryDsl`
               `&mut Paginated<T>: Table`
               which is required by `&mut Paginated<T>: QueryDsl`
    ```

---
+++ src/bin/extending_pager.rs
@@ -0,0 +1,67 @@
+//use diesel::backend::{BindCollector, QueryBuilder};
+// use diesel::impl_query_id; // deprecated since 1.1.0
+use diesel::pg::Pg;
+use diesel::query_builder::{AsQuery, AstPass, Query, QueryFragment};
+use diesel::sql_types::BigInt;
+use diesel::Expression;
+use diesel::{PgConnection, QueryDsl, QueryResult, RunQueryDsl};
+use diesel::{QueryId, Queryable};
+use diesel_demo::schema::posts;
+
+// Implement `QueryFragment`
+impl<T> QueryFragment<Pg> for Paginated<T>
+where
+    T: QueryFragment<Pg>,
+{
+    fn walk_ast(&self, mut out: AstPass<Pg>) -> QueryResult<()> {
+        out.push_sql("select *, count(*) over () from (");
+        self.query.walk_ast(out.reborrow())?;
+        out.push_sql(") LIMIT ");
+        out.push_bind_param::<BigInt, _>(&self.limit())?;
+        out.push_sql(" OFFSET ");
+        out.push_bind_param::<BigInt, _>(&self.offset())?;
+        Ok(())
+    }
+}
+
+// Whenever you implement `QueryFragment` you need to implement `QueryId`
+impl<T: Query> Query for Paginated<T> {
+    type SqlType = (T::SqlType, BigInt);
+}
+
+impl<T> RunQueryDsl<PgConnection> for Paginated<T> {}
+
+// FIXME: maybe implement this? but not this way?
+// impl<T> QueryDsl for Paginated<T> {}
+
+// Using `trait Paginate` to implement all that
+pub trait Paginate: AsQuery + Sized {
+    fn paginate(self, page: i64) -> Paginated<Self::Query> {
+        Paginated {
+            query: self.as_query(),
+            page,
+            per_page: DEFAULT_PER_PAGE,
+        }
+    }
+}
+
+impl<T: AsQuery> Paginate for T {}
+const DEFAULT_PER_PAGE: i64 = 10;
+
+#[derive(Queryable, QueryId, Debug)]
+pub struct Paginated<T> {
+    query: T,
+    page: i64,
+    per_page: i64,
+}
+
+impl<T> Paginated<T> {
+    pub fn per_page(self, per_page: i64) -> Self {
+        Paginated { per_page, ..self }
+    }
+}
+
+fn main() {
+    let second = posts::table.paginate(3).per_page(2);
+    println!("count hay {:?}", &second);
+}
